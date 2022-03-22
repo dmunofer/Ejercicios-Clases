4898e5313efeec0b5f78e3b690826de50a011871
dmunofer <diego.fmz@hotmail.com>
2022-03-22 11:22:50 +0100

    puzzle

---
+++ Puzzle.py
@@ -0,0 +1,26 @@
+class A: 
+    def z(self): 
+        return self 
+ 
+    def y(self, t): 
+        return len(t) 
+ 
+a = A 
+y = a.z 
+print(y(a)) 
+aa = a() 
+print(aa is a()) 
+z = aa.y 
+print(z(())) 
+print(a().y((a,))) 
+print(A.y(aa, (a,z))) 
+print(aa.y((z,1,'z'))) 
+
+'''
+<class '__main__.A'>  #Aqui no imprime lo que pedimos porque no hay un str en la clase
+False
+0
+1
+2
+3
+'''
