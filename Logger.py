cef88f2307241aff383fdfd351b4df256230c11f
dmunofer <diego.fmz@hotmail.com>
2022-03-22 11:28:33 +0100

    clase logger

---
+++ Logger.py
@@ -0,0 +1,11 @@
+class Logger():
+    def __init__(self,mensaje):
+        self.mensaje=mensaje
+    def __str__(self):
+        cadena = '--Start log--'
+        for i in range (1,6):
+            cadena += '/n '+i +'ªllamada'
+
+        cadena+= '/n --End log:'+ '5 log(s)--'
+
+
