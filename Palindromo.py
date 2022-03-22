b62128ac17535c1376d128d709db238ed7391886
dmunofer <diego.fmz@hotmail.com>
2022-03-22 11:19:20 +0100

    a y b con str

--- Palindromo.py
+++ Palindromo.py
@@ -2,16 +2,23 @@ class Palindromo():
 
     def __init__(self,palindromo):
         self.palindromo = palindromo
-    def __str__(self):
-        return es_palindromo(self.palindromo)
 
-    def es_palindromo(palabra):                    # Reutilizamos el codigo de la funcion palindromo de los ejercicicos de recursividad
+    def __str__(self):
+        return str(self.palindromo.upper())
+
+    @staticmethod
+    def esPalindromo(palabra):                    # Reutilizamos el codigo de la funcion palindromo de los ejercicicos de recursividad
         a,b = 'áéíóúüñÁÉÍÓÚÜ','aeiouunAEIOUU'
         palabra = palabra.replace(" ","")
         trans = str.maketrans(a,b)
         string = palabra.translate(trans)
+
         if len(string)<2:
             return True
         elif string.lower()[0]!=string.lower()[-1]:
             return False
-        return es_palindromo(string[1:-1])
+        return esPalindromo(string[1:-1])         # Aqui no entiendo porque no reconoce la funcion esPalindromo
+
+    def test(self):
+        booleano = esPalindromo(self.palindromo)
+        return booleano
